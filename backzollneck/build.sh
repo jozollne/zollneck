67ec69a3c39f321a7e4aacd4d28117b31ee5aafd
John Zollner <j.zollner04@gmail.com>
2024-05-28 13:48:51 +0200

    ready for new location /op

--- backzollneck/build.sh
+++ backzollneck/build.sh
@@ -1,5 +1,5 @@
 npm run build
-pm2 stop /zollneck/backzollneck/ecosystem.config.js
+pm2 stop /opt/zollneck/backzollneck/ecosystem.config.js
 sudo rm -rf /var/www/zollneck.de/back/dist
 sudo cp -r ./dist /var/www/zollneck.de/back/
 sudo cp -r ./node_modules /var/www/zollneck.de/back/
